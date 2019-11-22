6a5fcc6e7d9850fd4bc863b97f2fa04e3c92665b
anyong12345 <58073653+anyong12345@users.noreply.github.com>
2019-11-22 20:37:14 +0900

    pratikum 5

---
+++ pgame16073/code5.5.py
@@ -0,0 +1,12 @@
+A=50
+B=20
+
+if A > B:
+	print("A lebih Besar dari B")
+if A > B:
+	print("A sama dengan B")
+elif A == B:
+	print ("A sama dengan B")
+else:
+	print("A lebih kecil dari B")
+© 2019 GitHub, Inc.
